704d393258f8a22fde6062b70a0b81d02842d162
Deepak98 <Deepakvybu@gmail.com>
2022-03-28 15:38:05 +0530

    done

---
+++ src/test/java/MainClass.java
@@ -0,0 +1,14 @@
+import org.test.BaseClass;
+import org.testng.annotations.Test;
+
+public class MainClass  extends BaseClass{
+	
+	
+	@Test
+	private void Record() {
+		launchBrowser();
+		passUrl("");
+
+	}
+
+}
